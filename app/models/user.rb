0847056630bffecd5cef6f4576295172eb98e0f7
paramagician <konnor5456@gmail.com>
2018-11-15 22:43:11 +0000

    added email tests and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  # full explanation @ https://www.railstutorial.org/book/modeling_users#table-valid_email_regex
+  # rubular.com contains useful regex editing
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+
+  # original VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 end
