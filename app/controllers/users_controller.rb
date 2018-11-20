54153d54ad217a92e9dee63e0f8b94d9635ebd04
konnor <konnor5456@gmail.com>
2018-11-20 17:05:46 -0500

    added user profile page, finished @figure 7.7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new; end
+  def new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # adds debugging via byebug gem
+    # debugger
+  end
 end
