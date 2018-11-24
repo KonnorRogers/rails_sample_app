6ca0b069ea6538f08b5d51a28bdadfdbb125ebc2
paramagician <konnor5456@gmail.com>
2018-11-24 05:48:22 +0000

    Added permenant session cookies to keep persistent logins. Finished @ 9.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -19,9 +21,34 @@ class User < ApplicationRecord
   has_secure_password # uses bcrypt
   validates :password, presence: true, length: { minimum: 6 }
 
+  # Methods
+
+  # Creates a hashed cookie
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Creates a secure token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # remembers a user
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # authenticates
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -34,12 +34,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # Simulate a user clicking logout in another browser
+    delete logout_path
     follow_redirect!
 
     assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', logout_path, count: 0
     assert_select 'a[href=?]', user_path(@user), count: 0
-
     assert_not flash.empty?
+
   end
 end
