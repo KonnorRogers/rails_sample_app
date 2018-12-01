3058d16ba74c5635d20632d84f901e810375ea68
paramagician <konnor5456@gmail.com>
2018-12-01 06:57:22 +0000

    finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[edit update index]
+  before_action :logged_in_user, only: %i[edit update index destroy]
   before_action :correct_user, only: %i[edit update]
-  # Only acts on :edit & :update actions
+  before_action :admin_user, only: :destroy
 
   def new
     @user = User.new
@@ -45,13 +45,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = 'User deleted'
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit(
       :name, :email,
-      :password, :password_confirmation,
-      :admin
+      :password, :password_confirmation
     )
   end
 
@@ -71,5 +76,9 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
