346c99530ca45e7efa8149d307c9439e3146e71d
paramagician <konnor5456@gmail.com>
2018-12-07 16:29:40 +0000

    finished chapter 12, password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,20 +1,65 @@
+# frozen_string_literal: true
+
 class PasswordResetsController < ApplicationController
-  def new
-  end
+  before_action :get_user, only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+  before_action :check_expiration, only: %i[edit update] # case 1
+
+  def new; end
 
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
+      flash[:info] = 'Email sent with password reset instructions'
       redirect_to(root_url)
     else
-      flash.now[:danger] = "Email address not found"
+      flash.now[:danger] = 'Email address not found'
       render 'new'
     end
   end
 
-  def edit
+  def edit; end
+
+  def update
+    if params[:user][:password].blank? # case 3
+      @user.errors.add(:password, 'Cant be blank')
+      render 'edit'
+    elsif @user.update_attributes(user_params) # case 4
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = 'Password has been reset.'
+      redirect_to(@user)
+    else
+      render 'edit' # case 2
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  # BEFORE FILTERS
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # Confirms a valid user
+  def valid_user
+    return if @user&.activated? && @user&.authenticated?(:reset, params[:id])
+
+    redirect_to(root_url)
+  end
+
+  # Checks expiration of reset token
+  def check_expiration
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = 'Password reset has expired.'
+    redirect_to(new_password_reset_url)
   end
 end
