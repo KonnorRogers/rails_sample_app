e4f03e9a960dc74535a01bdb3522ba75678ec928
paramagicdev <konnor5456@gmail.com>
2018-12-15 05:16:15 +0000

    finished micropost model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,9 @@
 class Micropost < ApplicationRecord
   belongs_to :user
 
+  # Automatically pulls from DB in descending order using a Proc (lambda)
+  default_scope -> { order(created_at: :desc) }
+
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
