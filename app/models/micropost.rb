da64b3dae5bc1e4bb6330098f274941e8d69026f
paramagicdev <konnor5456@gmail.com>
2018-12-29 07:04:52 +0000

    Finished at creating AWS account for image upload

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,17 @@ class Micropost < ApplicationRecord
 
   # Automatically pulls from DB in descending order using a Proc (lambda)
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader # image upload
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+
+  private
+
+  # Validates the size of a picture
+  def picture_size
+    errors.add(:picture, 'should be less than 5MB') if picture.size > 5.megabytes
+  end
+
 end
