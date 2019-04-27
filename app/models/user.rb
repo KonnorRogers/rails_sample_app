cc8c9898a06f876cec1b1719e5bb6080a0576e7b
paramagicdev <konnor5456@gmail.com>
2019-04-27 02:30:47 -0400

    added active and passive relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,23 @@
 
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+
+  # defines relationships where the user is following somebody else
   has_many :active_relationships, class_name: 'Relationship',
                                   foreign_key: 'follower_id',
                                   dependent: :destroy
+
+  # to get a list of who a user is following, user.following, this is achieved
+  # via the source method
   has_many :following, through: :active_relationships, source: :followed
+
+  # defines relationships where the user is followed by somebody else
+  has_many :passive_relationships, class_name: 'Relationship',
+                                   foreign_key: 'followed_id',
+                                   dependent: :destroy
+
+  # allows calling of user.followers via a join table
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
