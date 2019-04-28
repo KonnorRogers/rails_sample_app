513e23fc890f76400f65141178c71c66e721db38
paramagicdev <konnor5456@gmail.com>
2019-04-27 23:58:20 -0400

    finished @ listing 14.16

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,11 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   # %i[] is the same as [:foo, :bar]
   resources :password_resets, only: %i[new create edit update]
